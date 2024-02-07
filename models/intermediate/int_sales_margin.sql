8b9b0a1e53d33f5f083b6dfb2e0a46a25b85b704
Clement Tome <clmnocoder@gmail.com>
2024-02-07 10:50:43 +0000

    margin et purchase_cost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,9 +1,9 @@
 SELECT
 products_id,
-purchase_price,
-quantity,
-revenue
+ROUND(SUM(CAST(purchase_price AS FLOAT64)* quantity)) AS purchase_cost,
+ROUND(SUM(revenue - (CAST(purchase_price AS FLOAT64) * quantity))) AS margin
 FROM {{ref('stg_raw__sales')}}
 JOIN {{ref('stg_raw__product')}}
 USING (products_id)
+GROUP BY products_id
 ORDER BY products_id
